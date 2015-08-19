25e36e99228e130ac1a05eeeb9874a01e626752f
Jordan Matelsky <j6k4m8@gmail.com>
2015-08-19 16:09:20 -0400

    Add exchange, improve download routers

--- client/javascripts/_utils.js
+++ client/javascripts/_utils.js
@@ -5,3 +5,7 @@ UI.registerHelper('title', function(t) {
 UI.registerHelper('contact_email_link', function() {
     return '<a href="mailto:contact@ocp.me">contact@ocp.me</a>';
 });
+
+UI.registerHelper('ocp_github', function(repo) {
+    return "https://github.com/openconnectome/" + repo + "/"
+});
